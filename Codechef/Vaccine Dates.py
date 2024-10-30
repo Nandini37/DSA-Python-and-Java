86f28f05d46b06422b94434c2ee58f303b15bfb2
Nandini Wadaskar <94116857+Nandini37@users.noreply.github.com>
2024-10-30 21:09:01 +0530

    Create Vaccine Dates.py

---
+++ Codechef/Vaccine Dates.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+while t > 0:
+    s = input("")
+    arr = s.split(" ")
+    
+    D = int(arr[0])
+    L = int(arr[1])
+    R = int(arr[2])
+    
+    if D<L:
+        print("Too Early")
+    elif D>R:
+        print("Too Late")
+    else:
+        print("Take second dose now")
+        
+    t = t-1
